f1dffec2932c8e75982e08a316d0f473f26ee20e
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-14 16:40:14 -0600

    style contact form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import ContactForm from "./components/Contact";
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-        <About></About>
+      <ContactForm></ContactForm>
+        {!contactSelected ? (
+          <>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
