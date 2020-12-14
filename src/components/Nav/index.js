5bc45721ef5955fe18b887b7c24d0887fd908b63
Jeff Johnston <jeffwjohn@yahoo.com>
2020-12-14 15:06:59 -0600

    add Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Nav() {
+
+  return (
+    <header>
+                <h1>Jeff Johnston</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#bio">About Me</a>
+                </li>
+                <li>
+                    <a href="#work">Work</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact Me</a>
+                </li>
+                <li>
+                    <a href="../../assets/documents/CV-Resume-2020-Jeffery-Johnston-TECH.pdf">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Nav;
